1cc2cedc7eee4c6c9f9df4e4e04d972ced7790f4
BPChance <bradenpchance6@gmail.com>
2025-06-11 22:57:56 -0500

    fixed vote button

--- components/VoteButton.tsx
+++ components/VoteButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { HandThumbUpIcon } from "@heroicons/react/24/outline";
 import { addVote } from "@/lib/actions";
 
@@ -6,6 +8,9 @@ export default function VoteButton({ id }: { id: string }) {
     <form action={addVote}>
       <input type="hidden" name="id" value={id} />
       <button
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
         type="submit"
         className="h-8 w-8 min-w-[2rem] rounded-full ring-gray-200 hover:text-atlas-teal active:bg-primary active:text-white active:outline-none active:ring-2 active:ring-primary"
       >
